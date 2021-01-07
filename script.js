7b3a662f498c3aded6f616a9dea202ed86f1f79a
Daniel <daniel.castelline@gmail.com>
2021-01-07 09:56:08 -0600

    added question layout to js

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 // Variables needed
 var beginBtn = document.getElementById("begin-button")
+
+// Questions
+var questions = [
+    {
+        question: "",
+        choices: [],
+        ansewer: ""
+    },
+    {
+        question: "",
+        choices: [],
+        ansewer: ""
+    },
+    {
+        question: "",
+        choices: [],
+        ansewer: ""
+    },
+    {
+        question: "",
+        choices: [],
+        ansewer: ""
+    },
+    {
+        question: "",
+        choices: [],
+        ansewer: ""
+    },
+
+]
