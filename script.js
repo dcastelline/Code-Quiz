6bc23974890b9635a461d037a3c764be849c509b
Daniel <daniel.castelline@gmail.com>
2021-01-07 19:40:44 -0600

    added user choices div to html, more variables in js

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 // Variables needed
-var beginBtn = document.getElementById("begin-button")
+// Buttons
+var startBtn = document.getElementById("start-btn")
+var submitScore = document.getElementById("score-btn")
+// Quiz Block
+var quizBlock = document.getElementById("quiz-block")
+// Time
+var timer = document.getElementById("timer")
+var lostTime = 10;
+var timeLeft = 60;
+var timeInterval = 0;
+// Score
+var score = 0;
+// Question asked tracker
+var questionAsked = 0;
+// New element shortcut
+var newEl = document.createElement("ul");
 
-// Questions
+//Questions
 var questions = [
     {
-        question: "",
-        choices: [],
-        ansewer: ""
+        question: "What does HTML stand for?",
+        choices: ["Hypertext Markup Language", "Hyper Text Marking Language", "Hypertrophy Marketing Language", "Hypertron Module Language"],
+        answer: "Hypertext Markup Language"
     },
     {
-        question: "",
-        choices: [],
-        ansewer: ""
+        question: "What does CSS stand for?",
+        choices: ["Cascade Style State", "Cascading Sheets Styling", "Cascading Style Sheets", "Cascade Style Sheet"],
+        answer: "Cascading Style Sheets"
     },
     {
-        question: "",
-        choices: [],
-        ansewer: ""
+        question: "Which of the following HTML element types are self closing?",
+        choices: ["div", "script", "img", "All of the Above" ],
+        answer: "img"
     },
     {
-        question: "",
-        choices: [],
-        ansewer: ""
+        question: "Which of the following is a JavaScript library?",
+        choices: ["jQuery", "Bootstrap", "Bootswatch", "Alexandria"],
+        answer: "jQuery"
     },
     {
-        question: "",
-        choices: [],
-        ansewer: ""
+        question: "Which of the following is a way to declare a variable in JavaScript?",
+        choices: ["$var", "vari", "$var =", "var"],
+        answer: "var"
     },
-
-]
+    {
+        question: "What is the airspeed velocity of an unladen swallow?"
+        choices: ["24mph", "32mph", "15mph", "African or European?"]
+        answer: "African or European?"
+    }
